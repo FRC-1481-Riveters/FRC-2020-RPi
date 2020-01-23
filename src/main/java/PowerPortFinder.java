d4dfbd68d44d47e814741106bdd6d2d71f576aca
dakott@pobox.com <dakott@pobox.com>
2020-01-23 16:51:26 -0500

    Added ability to sort PowerPortTargets. Added aimPoint to support future robot targeting.  Improved prototype PowerPort contour with a cleaner trace for better coordinates.

--- src/main/java/PowerPortFinder.java
+++ src/main/java/PowerPortFinder.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.opencv.core.*;
@@ -8,21 +9,21 @@ import org.opencv.imgproc.*;
 public class PowerPortFinder extends PowerPort {
 
     private Mat m_lastImage;
-    private Mat m_prototypePowerPortContour;
-    private double m_maxMatchThreshold = 500.0;
+    private MatOfPoint m_prototypePowerPortContour;
+    private double m_maxMatchThreshold = 15.0;
+
 
     public PowerPortFinder() {
 
-        m_prototypePowerPortContour = new Mat(20, 1, CvType.CV_32FC2);
         /*
-         * This is a simplified 20 point representation of a Power Port target. The
-         * first two numbers are the index of the matrix that's being initialized: 0, 0
-         * The following numbers are coordinate pairs of x and y, e.g. (39, 40) to (132,
-         * 197) are two points on the contour. They have a line segment between them.
+         * This is a simplified 8 point representation of a Power Port target. The first
+         * two numbers are the index of the matrix that's being initialized: 0, 0 The
+         * following numbers are coordinate pairs of x and y, e.g. (55, 39) to (99, 39)
+         * are two points on the contour. They have a line segment between them.
          */
-        m_prototypePowerPortContour.put(0, 0, 39, 40, 132, 197, 148, 230, 161, 247, 182, 287, 189, 289, 477, 287, 494,
-                263, 509, 231, 515, 226, 576, 115, 582, 110, 620, 44, 620, 39, 592, 39, 462, 266, 199, 266, 200, 262,
-                194, 257, 70, 39);
+        m_prototypePowerPortContour = new MatOfPoint(new Point(55, 39), new Point(99, 39), new Point(260, 320),
+                new Point(585, 320), new Point(745, 39), new Point(790, 39), new Point(605, 357), new Point(236, 357),
+                new Point(55, 39));
 
     }
 
@@ -39,9 +40,9 @@ public class PowerPortFinder extends PowerPort {
         return m_lastImage;
     }
 
-    private ArrayList<PowerPortTarget> processPowerPortContours(List<MatOfPoint> contours) {
+    private List<PowerPortTarget> processPowerPortContours(List<MatOfPoint> contours) {
 
-        ArrayList<PowerPortTarget> PowerPortTargets = new ArrayList<>();
+        List<PowerPortTarget> PowerPortTargets = new ArrayList<>();
 
         for (MatOfPoint contour : contours) {
             double matchStrength = Imgproc.matchShapes(m_prototypePowerPortContour, contour, Imgproc.CONTOURS_MATCH_I3,
@@ -56,24 +57,51 @@ public class PowerPortFinder extends PowerPort {
                      */
                     double x = (p.get_m10() / p.get_m00());
                     double y = (p.get_m01() / p.get_m00());
+                    
+                    // TODO: Make the aimPoint better by targeting the middle of the line between the tips of the top of the contour.
 
-                    PowerPortTargets.add(new PowerPortTarget(contour, new Point(x, y), matchStrength));
+                    double x_aimPoint = x;
+                    double y_aimPoint = y;
+                                       
+                    
+                    PowerPortTargets.add(new PowerPortTarget(contour, new Point(x, y), new Point (x_aimPoint, y_aimPoint), matchStrength));
                 } catch (Exception ex) {
 
                 }
             }
         }
+        
+        Collections.sort(PowerPortTargets);
 
         return PowerPortTargets;
     }
 
     public ArrayList<PowerPortTarget> getPowerPorts() {
 
-		ArrayList<PowerPortTarget> PowerPorts = new ArrayList<PowerPortTarget>();
+        ArrayList<PowerPortTarget> PowerPorts = new ArrayList<PowerPortTarget>();
 
-		PowerPorts.addAll(processPowerPortContours(filterContoursOutput()));
+        PowerPorts.addAll(processPowerPortContours(filterContoursOutput()));
 
-		return PowerPorts;
+        return PowerPorts;
+    }
+    
+	/*
+	 * Rotated rectangle angles are weird. See
+	 * https://namkeenman.wordpress.com/2015/12/18/open-cv-determine-angle-of-
+	 * rotatedrect-minarearect/ for more details
+	 * 
+	 * This normalizes it to a nominal polar framework, where the 0 degrees is
+	 * parallel to the x axis poing to the right. 180 is parallel to the x axis
+	 * pointing to the left.
+	 */
+	double getAdjustedAngle(RotatedRect rRect) {
+		double angle;
+		if (rRect.size.width < rRect.size.height) {
+			angle = 90 - rRect.angle;
+		} else {
+			angle = -rRect.angle;
+		}
+
+		return angle;
 	}
-
 }
--- src/main/java/PowerPortTarget.java
+++ src/main/java/PowerPortTarget.java
@@ -1,17 +1,32 @@
 
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
-import org.opencv.core.Scalar;
 
-public class PowerPortTarget {
+
+public class PowerPortTarget implements Comparable<PowerPortTarget> {
 	public MatOfPoint contour;
 	public Point center;
+	public Point aimPoint;
 	public double confidence;
 
 
-	public PowerPortTarget(MatOfPoint contour, Point center, double confidence) {
+	public PowerPortTarget(MatOfPoint contour, Point center, Point aimPoint, double confidence) {
 		this.contour = contour;
 		this.center = center;
 		this.confidence = confidence;
+		this.aimPoint = aimPoint;
 	}
+
+
+	@Override
+	public int compareTo(PowerPortTarget o) {
+		if (this.confidence > o.confidence) {
+			return 1;
+		}
+		if (this.confidence < o.confidence) {
+			return -1;
+		}
+		return 0;
+	}
+
 }
